984d6e8a9f7f33c060fb357ecbafacbfd24bed73
Chris Hallberg <chris.hallberg@hey.com>
2022-10-03 12:17:26 -0400

    Add @astro/image.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+  ]
 });
